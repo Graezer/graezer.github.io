950624409d6857180d6a5f70264b4c60518b3a2f
geza steinert <gwasteinert@gmail.com>
2022-07-09 19:05:13 +0200

    add basic styling to main page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 function App() {
   return (
-  <div>
-    <Link to="/about">About</Link>
-    <h2>Github Pages</h2>
-    <h3>Deploying React to Github</h3>
-  </div>
+    <div className='m-3 p-3'>
+      <Link className='link-secondary text-decoration-none' to="/about" >About</Link>
+      <h2>PLEASE SHOW UP</h2>
+      <h3>Deploying React to Github</h3>
+    </div>
   );
  }
  export default App;
