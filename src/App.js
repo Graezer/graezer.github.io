f9dcd789f1ecafa4a871cd7b1c95f9e0c79ebc6a
geza steinert <gwasteinert@gmail.com>
2022-07-11 19:27:31 +0200

    add styling to pictures in caroussel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Link } from "react-router-dom";
 
 function App() {
+
   return (
     <div className='m-3 p-3'>
       <Link className='link-secondary text-decoration-none' to="/about" >About</Link>
@@ -16,13 +17,13 @@ function App() {
         </div>
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src="https://www.schauspielervideos.de/FM/fotos/14/99014058/kf1745261_gr_1800.jpg" className="d-block w-100" alt="..." />
+            <img src="https://www.schauspielervideos.de/FM/fotos/14/99014058/kf1745261_gr_1800.jpg" className="d-block" alt="..." />
           </div>
           <div className="carousel-item">
-            <img src="https://d205pfv3qf1itp.cloudfront.net/s3/castupload-com/production/294fa79f-438b-4b38-97b9-5dd6faa4b1c6.jpg/b4d4d92b1dfb6b5e2ce837e612d8200af43554ad6c767a38d24917d4c19fec17/c_crop,f_jpg,h_4155,q_80,w_4155,x_0,y_212/c_fill,g_North,h_500,w_500" className="d-block w-100" alt="..." />
+            <img src="https://d205pfv3qf1itp.cloudfront.net/s3/castupload-com/production/294fa79f-438b-4b38-97b9-5dd6faa4b1c6.jpg/b4d4d92b1dfb6b5e2ce837e612d8200af43554ad6c767a38d24917d4c19fec17/c_crop,f_jpg,h_4155,q_80,w_4155,x_0,y_212/c_fill,g_North,h_500,w_500" className="d-block " alt="..." />
           </div>
           <div className="carousel-item">
-            <img src="https://www.schauspielervideos.de/FM/fotos/14/99014058/kf1745264_gr_1800.jpg" className="d-block w-100" alt="..." />
+            <img src="https://www.schauspielervideos.de/FM/fotos/14/99014058/kf1745264_gr_1800.jpg" className="d-block" alt="..." />
           </div>
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
